d0193a2b3df15bb80ce22173b90451145434a7f6
Hyeonkyeong Park <park0600@algonquinlive.com>
2024-07-20 16:53:10 -0400

    Import Array data from notifications.js and display as a list.

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,30 @@ import { useState } from 'react'
 import './App.css'
 import { Title } from "./components/Title"
 import Notification from './components/Notification/Notification'
+import notifications from './notifications';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [displayNotifications, setDisplayNotifications] = useState(notifications)
+
+  const handle
 
   return (
     <>
       <Title title="Notification Lists" />
+      <button>Clear All</button>
       <div className='container'>
-        <Notification></Notification>
+          {displayNotifications.map((notifi, index) => (
+            <Notification key={index}>
+                <div className="content">
+                    <h4 className="name">{notifi.name}</h4>
+                    <p className="message">{notifi.message}</p>
+                </div>
+                <div className="container-btn">
+                    <button className="delete-btn">Delete</button>
+                </div>
+            </Notification>
+          ))}
       </div>
     </>
   )
--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -4,14 +4,6 @@ const Notification = ({children}) => {
     return (
         <div className="container-notification">
             {children}
-            <h3 className="id">id</h3>
-            <div className="content">
-                <h4 className="name">name</h4>
-                <p className="message">message</p>
-            </div>
-            <div className="container-btn">
-                <button className="delete-btn">Delete</button>
-            </div>
         </div>
     )
 } 
